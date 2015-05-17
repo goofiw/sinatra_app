5cba1a71465989ead9a9fe97667340da9909258e
goofiw <goofiw@gmail.com>
2015-05-17 00:50:39 +0000

    working app for posting items

--- app/actions.rb
+++ app/actions.rb
@@ -5,5 +5,24 @@ end
 
 get '/messages' do
   @messages = Message.all
-  erb :'index'
+  erb :index
+end
+
+get '/messages/new' do
+	erb :'messages/new'
+end
+
+post '/messages' do
+		@message = Message.new(
+		         title: params[:title],
+		         content: params[:content],
+		         author: params[:author]
+		            )
+	@message.save
+	redirect '/messages'
+end
+
+get '/messages/:id' do
+	@message = Message.find params[:id]
+	erb :'messages/show'
 end
