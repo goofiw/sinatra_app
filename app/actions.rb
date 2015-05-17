32ee3d761cdcc639b72a6c7c9a707bbe52aa246b
goofiw <goofiw@gmail.com>
2015-05-17 20:12:48 +0000

    No title - functioning

--- app/actions.rb
+++ app/actions.rb
@@ -14,9 +14,8 @@ end
 
 post '/messages' do
 		@message = Message.new(
-		         title: params[:title],
-		         content: params[:content],
-		         author: params[:author]
+		         author: params[:author],
+		         content: params[:content]
 		            )
 	if @message.save
 		redirect '/messages'
