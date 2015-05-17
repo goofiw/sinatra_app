5ed3b4a92c5672b831c0d7fe92c554c35721ff6e
goofiw <goofiw@gmail.com>
2015-05-17 20:51:07 +0000

    added timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150517202158) do
+ActiveRecord::Schema.define(version: 20150517203815) do
 
   create_table "messages", force: true do |t|
-    t.string "author"
-    t.string "content"
-    t.string "url"
+    t.string   "author"
+    t.string   "content"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
